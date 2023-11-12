7f06ee7c438687be14f5c09a7111871bb049a9d0
Bintang Rezeka Ramadani <bintangrezeka@gmail.com>
2023-11-12 14:30:41 +0700

    remove Python interpreter

--- ecosystem.config.js
+++ ecosystem.config.js
@@ -3,7 +3,6 @@ module.exports = {
     {
       name: "simple-wallet-ocr",
       cmd: "gunicorn --bind 0.0.0.0:8080 wsgi:app",
-      interpreter: "python3",
       autorestart: true,
       watch: true,
       ignore_watch: ["images"],
